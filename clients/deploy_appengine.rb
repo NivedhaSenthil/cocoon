9aa58dbc93526a0f9b3889e35466c99414043195
Nivedha <nivedhasenthil@gmail.com>
2021-10-21 20:21:48 +0530

    clean up by arranging actions in methods

--- clients/deploy_appengine.rb
+++ clients/deploy_appengine.rb
@@ -1,20 +1,44 @@
 require "google/cloud/resource_manager"     
 require "google/cloud/billing"
 
-resource_manager = Google::Cloud::ResourceManager.new
-billing_account_name = ""
-project_id = ""
-project_name = ""
+def create_project()
+    project_name = ENV["COCOON_PROJECT_ID"]
 
-project = resource_manager.create_project project_id,
-                                          name: project_name
+    raise "project name is needed to create a GCP project" if project_name.nil?
 
-billing_manager = Google::Cloud::Billing.cloud_billing_service   
-billing_manager.update_project_billing_info name: "projects/#{project_id}", 
-                                            project_billing_info: {
-                                                billing_account_name: "billingAccounts/#{billing_account_name}",
-                                                billing_enabled: true,
-                                                project_id: project_id,
-                                                name: "projects/#{project_id}/billingInfo",
-                                            }
+    ENV["COCOON_PROJECT_ID"] = project_id = project_name.downcase.tr(" ", "-") + rand(10000).to_s
+
+    resource_manager = Google::Cloud::ResourceManager.new
+    project = resource_manager.create_project project_id,
+                                              name: project_name
+
+    p "Project created with id #{project_id}"                                              
+end    
+
+def enable_billing_account()
+    project_id = ENV["COCOON_PROJECT_ID"]
+    billing_account_name = ENV["COCOON_BILLING_ACCOUNT_ID"]
+
+    raise "project id is needed to create a GCP project" if project_id.nil? || billing_account_name.nil?
+
+    p "Enabling billing account for the project"
+
+    billing_manager = Google::Cloud::Billing.cloud_billing_service   
+    billing_manager.update_project_billing_info name: "projects/#{project_id}", 
+                                                project_billing_info: {
+                                                    billing_account_name: "billingAccounts/#{billing_account_name}",
+                                                    billing_enabled: true,
+                                                    project_id: project_id,
+                                                    name: "projects/#{project_id}/billingInfo",
+                                                }
+end    
+
+def main()
+    create_project()
+    enable_billing_account()
+end
+
+main()
+
+
 
--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+echo "Enter a project name:"
+read COCOON_PROJECT_ID
+
+echo "Enter billing account id to link with the new project:"
+read COCOON_BILLING_ACCOUNT_ID
+
 # create sample rails app
 rails new appengine_example
 cd appengine_example
+
+echo $(pwd)
 
 # create a sample controller
 rails generate controller Welcome index
@@ -19,8 +27,10 @@ sed "s/<secret_key>/$(bundle exec rails secret)/g" $COCOON_HOME/rails_templates/
 gcloud auth login
 
 # create a gcp project
-ruby $COCOON_HOME/clients/deploy_appengine.rb
+COCOON_PROJECT_ID=$COCOON_PROJECT_ID COCOON_BILLING_ACCOUNT_ID=$COCOON_BILLING_ACCOUNT_ID ruby $COCOON_HOME/clients/deploy_appengine.rb
 gcloud config set project $COCOON_PROJECT_ID
+
+echo "GCP project create successfully. Starting appengine deployment..."
 
 # deploy the app
 gcloud app create
